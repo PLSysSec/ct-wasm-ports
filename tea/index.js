8388c3d899cbf4b2a46a5c436cd7e28f1d58e04a
Deian Stefan <deian@cs.ucsd.edu>
2018-06-15 19:10:11 -0700

    basic TEA done

--- tea/index.js
+++ tea/index.js
@@ -7,12 +7,23 @@ const memory = new WebAssembly.Memory({initial:1});
 WebAssembly.instantiate(wasmBlob, {js: {memory}}).then(tea => {
   console.log(tea);
   const mem = new Uint32Array(memory.buffer);
-  mem[0] = 3;
-  mem[1] = 7;
-  console.log(mem);
+  //message
+  mem[0] = 0xdeadbeef;
+  mem[1] = 0xbeeff00d;
+  // key
+  mem[2] = 0xd34db33f;
+  mem[3] = 0xb33ff33d;
+  mem[4] = 0xf000ba12;
+  mem[5] = 0xdeadf00d;
   //
-  const add = tea.instance.exports.add;
-  console.log(add());
+  const encrypt = tea.instance.exports.encrypt;
+  const decrypt = tea.instance.exports.decrypt;
+  //
+  console.log(`${mem[0]},${mem[1]}`);
+  encrypt();
+  console.log(`${mem[0]},${mem[1]}`);
+  decrypt();
+  console.log(`${mem[0]},${mem[1]}`);
 }).catch( err => {
   console.log(err);
   console.log(memory)
--- tea/ref.c
+++ tea/ref.c
@@ -25,10 +25,18 @@ void tea_decrypt(uint32_t v[2], uint32_t k[4]) {
     v[0]=v0; v[1]=v1;
 }
 
+uint32_t wtf(uint32_t j) {
+  for(uint32_t i = 0; i <32; i++) {
+    j++;
+  }
+  return j;
+}
+
 int main() {
   uint32_t message[2] = { 0xdeadbeef, 0xbeeff00d };
   uint32_t key[4]     = { 0xd34db33f, 0xb33ff33d, 0xf000ba12, 0xdeadf00d };
   tea_encrypt(message, key);
+  printf("%d\n%d\n", message[0], message[1]);
   tea_decrypt(message, key);
   printf("Okay: %d?\n", message[0] == 0xdeadbeef &&
                         message[1] == 0xbeeff00d);
