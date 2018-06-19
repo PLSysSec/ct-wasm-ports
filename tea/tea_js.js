88f675f71ba734b2c59765a2168e623c87ca06da
Natalie Popescu <nataliepopescu@gmail.com>
2018-06-19 11:09:30 -0700

    made right shifts in js impl logical

--- tea/tea_js.js
+++ tea/tea_js.js
@@ -1,12 +1,12 @@
 exports.init = (cb) => {
   exports.encrypt = function (v, k) {
-    let v0=v[0], v1=v[1], sum=0, i;           /* set up */
+    let v0=v[0], v1=v[1], sum=0, i, temp;           /* set up */
     const delta=0x9e3779b9;                     /* a key schedule constant */
     let k0=k[0], k1=k[1], k2=k[2], k3=k[3];   /* cache key */
     for (i=0; i < 32; i++) {                       /* basic cycle start */
-      sum += delta;
-      v0 += ((v1<<4) + k0) ^ (v1 + sum) ^ ((v1>>5) + k1);
-      v1 += ((v0<<4) + k2) ^ (v0 + sum) ^ ((v0>>5) + k3);
+      sum = (sum + delta)>>>0;
+      v0 = (v0 + (((v1<<4) + k0) ^ (v1 + sum) ^ ((v1>>>5) + k1)))>>>0;
+      v1 = (v1 + (((v0<<4) + k2) ^ (v0 + sum) ^ ((v0>>>5) + k3)))>>>0;
     }                                              /* end cycle */
     return [v0, v1];
   }
@@ -16,9 +16,9 @@ exports.init = (cb) => {
     const delta=0x9e3779b9;                     /* a key schedule constant */
     let k0=k[0], k1=k[1], k2=k[2], k3=k[3];   /* cache key */
     for (i=0; i<32; i++) {                         /* basic cycle start */
-      v1 -= ((v0<<4) + k2) ^ (v0 + sum) ^ ((v0>>5) + k3);
-      v0 -= ((v1<<4) + k0) ^ (v1 + sum) ^ ((v1>>5) + k1);
-      sum -= delta;
+      v1 = (v1 - (((v0<<4) + k2) ^ (v0 + sum) ^ ((v0>>>5) + k3)))>>>0;
+      v0 = (v0 - (((v1<<4) + k0) ^ (v1 + sum) ^ ((v1>>>5) + k1)))>>>0;
+      sum = (sum - delta)>>>0;
     }                                              /* end cycle */
     return [v0, v1];
   }
