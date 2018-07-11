abc6fd1d463ca689cbff548f6e58a218ef2a6424
John Renner <john@jrenner.net>
2018-07-11 01:32:27 -0700

    It's not pretty but it works

--- eval/node_tweetnacl/bench.sh
+++ eval/node_tweetnacl/bench.sh
@@ -1,8 +1,10 @@
-for nod in ct-node node; do
-  for nacl in ctwasm ctwasm-strip vanilla; do
-    echo "$nod $nacl"
-    if [ ! -f ./${nod}-${nacl}.bench ]; then
-      $nod $HOME/ct-wasm/ct-wasm-ports/eval/node_tweetnacl/test.js tn_${nacl}.wasm > ./${nod}-${nacl}.bench
-    fi
-  done ;
-done
+#! /usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+for nacl in ctwasm ctwasm-strip vanilla; do
+  $2 $DIR/test.js $DIR/tn_${nacl}.wasm > $1/ct-node-${nacl}.bench
+done ;
+
+for nacl in ctwasm ctwasm-strip vanilla; do
+  $3 $DIR/test.js $DIR/tn_${nacl}.wasm &> $1/node-${nacl}.bench
+done ;
